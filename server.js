44af83124567a14c9c1fe24f1bbfea7d2ca6fc0b
ndrishinski <ndrishinski@yahoo.com>
2019-11-12 20:07:40 -0700

    inital schema setup

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
     graphiql: true
-}))
+  })
+);
 
-app.listen(4000, () => console.log('running on port 4000'));
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
